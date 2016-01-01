64288b4c59c9a7de13148a93e568445876398675
Alex Ung <alexung.ucsb@gmail.com>
2015-12-31 23:27:06 -0800

    finished card

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -7,7 +7,7 @@ class CardsController < ApplicationController
 
   def password_entered
     if params["christmas_card"]["password"]
-      if params["christmas_card"]["password"] == "ilovemary"
+      if params["christmas_card"]["password"] == "bahbz"
         redirect_to christmas_card_2015_path, notice: "Correct!"
       elsif params["christmas_card"]["password"] == "iloveyay"
         redirect_to christmas_yay_path, notice: "Correct!"
